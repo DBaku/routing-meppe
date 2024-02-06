922030ccd58fd6220761ac60120f79794710d98a
DBaku <dimaba487@gmail.com>
2024-02-06 13:20:32 +0100

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@
 import "./App.css";
 
 // const [count, setCount] = useState(0)
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
